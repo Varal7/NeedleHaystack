8af504377e4736a9bcff7d9e306c178c64b13452
Varal7 <varaal@gmail.com>
2015-12-21 03:14:58 +0100

    Added beautiful output

--- Main.java
+++ Main.java
@@ -5,12 +5,21 @@ import java.io.IOException;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel.MapMode;
 import java.nio.charset.Charset;
+import java.lang.Math;
+
 
 public class Main {
 
+    public static final int precedingChars = 50; //Only used for esthetic purposes
+    public static final int displayedChars = 500; //Only used for esthetic purposes
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+
     public static void main (String[] args) {
-        String hFileName = "source.txt"; //Fichier texte à parcourir
-        String nFileName = "query.txt"; //Chaîne à rechercher
+        String hFileName = "source.txt"; //Text file to search in
+        String nFileName = "query.txt"; //Text file to search for
         if (args.length >=1) {
             hFileName = args[0];
         }
@@ -28,6 +37,7 @@ public class Main {
         }
         else {
           System.out.println("Needle found at index: " + result);
+          printComparisons(result, haystack, needle);
         }
 
     }
@@ -49,4 +59,27 @@ public class Main {
         charBuffer.get(charArray);
         return charArray;
         }
+
+      static void printComparisons(int index, char[] haystack, char[] needle) {
+        /**
+         * Displays result of search in a beautiful way
+         *
+         **/
+         int hLength = haystack.length-1;
+         int nLength = needle.length-1;
+         int start = Math.max(0,index-precedingChars);
+         int bodyLength = Math.min(nLength, displayedChars-precedingChars);
+         int end = Math.min(hLength, start+displayedChars);
+         for (int i = start; i < index; i++)
+           System.out.print(haystack[i]);
+         System.out.print(ANSI_GREEN);
+         for (int i = index; i < index+bodyLength; i++)
+           System.out.print(haystack[i]);
+         System.out.print(ANSI_RESET);
+         for (int i = index+bodyLength; i < end ; i++)
+           System.out.print(haystack[i]);
+         System.out.println("");
+
+
+      }
 }
