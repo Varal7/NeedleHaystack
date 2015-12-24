1411e85cce69b223a40adbab0f75a1f43118f208
Grand Miaou <chloe@MacBook-d-APACHA-249.local>
2015-12-24 02:08:28 +0100

    continue le code mais pas fini ni teste

--- BoyerMoore.java
+++ BoyerMoore.java
@@ -6,32 +6,34 @@ public class BoyerMoore{
 	private char[] needle;
 	private int hLength;
 	private int nLength;
-	private LinkedList[] occurrences;
+	public ArrayList<ArrayList<Integer>> occurrences;
 	private int sizeAlphabet;
 	public int[] suffixe;
+	public int[] prefixe;
 	
-	public BoyerMoore(char[] s1, char[] s2, int l1, int l2, int sizeAlphabet){
+	public BoyerMoore(char[] s1, char[] s2, int l1, int l2, int size){
 	
 		this.haystack = s1;
 		this.needle = s2;
 		this.hLength = l1;
 		this.nLength = l2;
-		this.sizeAlphabet = sizeAlphabet;
+		this.sizeAlphabet = size;
 		
-		this.occurrences = new LinkedList[sizeAlphabet];
-		
-		for(int i=0; i<sizeAlphabet; i++)
-			occurrences[i] = new LinkedList();
+		this.occurrences = new ArrayList<ArrayList<Integer>>();
 			
 		this.suffixe = new int[l2];
+		this.prefixe = new int[l2];
 		
 	
 	}
 	
 	public void setOccurrences(){
+	
+		for(ArrayList L : occurrences)
+			L= new ArrayList<Integer>();
 		
 		for(int i=0; i<nLength; i++){
-			occurrences[needle[i]].addFirst(i);
+			occurrences.get(needle[nLength-1-i]).add(i,nLength-1-i);
 		}
 		
 	}
@@ -63,8 +65,8 @@ public class BoyerMoore{
 			
 		}
 		
-		int longueurActuelle = 0;
-		int minIndiceTraite = nLength;
+	//	int longueurActuelle = 0;
+	//	int minIndiceTraite = nLength;
 		
 		for(int j=nLength-2; j>=0; j--){
 			
@@ -73,10 +75,70 @@ public class BoyerMoore{
 				
 		}
 		
+	//la ou on a des -1 il faut remplir avec autchose.
+	
+		
 	
 	}
 	
+	public int search(){
 	
+		int offset = 0;
+		int comparedIndex = nLength-1;
+		int shift;
+		int mismatch;
+		char Char;
+		int nextSuffix;
+		
+		while(offset<=hLength-nLength){
+		
+			shift=1;
+			
+			mismatch=compare(offset);
+			if(mismatch==-1)
+				return offset;
+				
+			
+			//check bad character
+			shift = mismatch+1; //dans le cas ou le caractere de haystack nest pas dans needle
+			
+	//		Char = haystack[offset+mismatch];
+	//		int value = (int)Char;
+	//		
+	//		for(int index : occurrences.get(value){
+				
+	//			if(index<mismatch){
+	//				shift = mismatch-index;
+	//				break;
+	//			}
+	//		}
+			
+			//check good suffix
+			nextSuffix = suffixe[mismatch];
+			if(nextSuffix==-1){
+				nextSuffix=prefixe[mismatch];
+			}
+			if(shift<mismatch-nextSuffix)
+				shift=mismatch-nextSuffix;
+			
+			
+			
+		}
+		
+		return -1;
 	
+	}
+	
+	public int compare(int offset){
+	// compare la chaine needle avec haystack de droite à gauche
+		
+		for(int i=nLength-1; i>=0;i--){
+			if(needle[i]!=haystack[i+offset])
+				return(i);
+		}
+		
+		return -1;
+	
+	}
 
 }
